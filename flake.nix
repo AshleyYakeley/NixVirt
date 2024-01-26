8d2195a0a25067c30786414fd94a3c23029e5175
Ashley Yakeley <ashley@semantic.org>
2024-01-26 14:04:46 -0800

    virtdecalre: use nix store shebang

--- flake.nix
+++ flake.nix
@@ -13,11 +13,20 @@
     };
 
     outputs = { self, nixpkgs }:
+    let
+        pkgs = import nixpkgs {system = "x86_64-linux";};
+        pythonPkg = pkgs.python3;
+        virtdeclareFile = pkgs.runCommand "virtdeclare" {}
+            ''
+            sed -e "1s|.*|\#\!${pythonPkg}/bin/python3|" ${./virtdeclare} > $out
+            chmod 755 $out
+            '';
+    in
     {
         apps.x86_64-linux.virtdeclare =
         {
             type = "app";
-            program = ./virtdeclare;
+            program = "${virtdeclareFile}";
         };
     };
 }
