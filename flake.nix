a153f20b5e28fd9090421fad46e32397ee5c97e9
Ashley Yakeley <ashley@semantic.org>
2024-01-27 00:32:24 -0800

    NixOS module

--- flake.nix
+++ flake.nix
@@ -28,5 +28,60 @@
             type = "app";
             program = "${virtdeclareFile}";
         };
+
+        nixosModules.default = { config, lib, pkgs, ... }:
+        let
+            cfg = config.virtualisation.libvirtd;
+        in
+        {
+            options.virtualisation.libvirtd =
+            {
+                domains = lib.mkOption
+                {
+                    type = with lib.types; listOf (submodule
+                    {
+                        options =
+                        {
+                            hypervisor = lib.mkOption
+                            {
+                                type = str;
+                                default = "qemu:///system";
+                                description = "hypervisor connection URI";
+                            };
+                            definition = lib.mkOption
+                            {
+                                type = path;
+                                description = "path to definition XML";
+                            };
+                            state = lib.mkOption
+                            {
+                                type = types.enum [ "stopped" "running" ];
+                                default = "stopped";
+                                description = "state to put the domain in";
+                            };
+                            auto = lib.mkOption
+                            {
+                                type = bool;
+                                default = true;
+                                description = "set autostart to match state";
+                            };
+                        };
+                    });
+                    default = [];
+                    description = "libvirtd domains";
+                };
+            };
+
+            config = lib.mkIf cfg.enable
+            (let
+                mkCommands = {hypervisor,definition,state,auto}:
+                ''
+                    ${virtdeclareFile} --connect ${hypervisor} --define ${definition} --state ${state} ${if auto then "--auto" else ""}
+                '';
+            in
+            {
+                system.activationScripts.libvirtd-domains = lib.concatStrings (lib.lists.forEach cfg.domains mkCommands);
+            });
+        };
     };
 }
