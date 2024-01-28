7737339df3af988eb4b58ac95ce62dd204c56fcb
Ashley Yakeley <ashley@semantic.org>
2024-01-27 16:17:39 -0800

    homeModules.default

--- flake.nix
+++ flake.nix
@@ -29,6 +29,67 @@
             program = "${virtdeclareFile}";
         };
 
+        homeModules.default = { config, lib, pkgs, ... }:
+        let
+            cfg = config.virtualisation.libvirt;
+        in
+        {
+            options.virtualisation.libvirt = with lib.types;
+            {
+                enable = lib.mkOption
+                {
+                    type = bool;
+                    default = false;
+                    description = "Enable management of libvirt domains";
+                };
+                domains = lib.mkOption
+                {
+                    type = listOf (submodule
+                    {
+                        options =
+                        {
+                            hypervisor = lib.mkOption
+                            {
+                                type = str;
+                                default = "qemu:///session";
+                                description = "hypervisor connection URI";
+                            };
+                            definition = lib.mkOption
+                            {
+                                type = path;
+                                description = "path to definition XML";
+                            };
+                            state = lib.mkOption
+                            {
+                                type = types.enum [ "stopped" "running" ];
+                                default = "stopped";
+                                description = "state to put the domain in";
+                            };
+                            auto = lib.mkOption
+                            {
+                                type = bool;
+                                default = true;
+                                description = "set autostart to match state";
+                            };
+                        };
+                    });
+                    default = [];
+                    description = "libvirt domains";
+                };
+            };
+
+            config = lib.mkIf cfg.enable
+            (let
+                mkCommands = {hypervisor,definition,state,auto}:
+                ''
+                    ${virtdeclareFile} --connect ${hypervisor} --define ${definition} --state ${state} ${if auto then "--auto" else ""}
+                '';
+            in
+            {
+                home.activation.libvirt-domains = lib.concatStrings (lib.lists.forEach cfg.domains mkCommands);
+            });
+        };
+
         nixosModules.default = { config, lib, pkgs, ... }:
         let
             cfg = config.virtualisation.libvirt;
