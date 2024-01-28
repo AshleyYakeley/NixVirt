a9039689cc30b2b872b7e2db64879b9adc47fc67
Ashley Yakeley <ashley@semantic.org>
2024-01-28 00:29:37 -0800

    more domain XML

--- lib.nix
+++ lib.nix
@@ -2,13 +2,68 @@ pkgs:
 let
     xml = import ./xml.nix;
 
-    domainXML = with xml; domain: toText
+    yesno = t: if t then "yes" else "no";
+    onoff = t: if t then "on" else "off";
+
+    domainXML = with builtins; with xml; domain: toText
     (
         elem "domain" {type = domain.type;}
         [
             (elem "name" {} domain.name)
             (elem "uuid" {} domain.uuid)
             (elem "title" {} domain.title)
+            (elem "metadata" {} domain.metadata)
+            (elem "memory" {unit="KiB";} domain.memory)
+            (elem "currentMemory" {unit="KiB";} domain.currentMemory)
+            (elem "vcpu" {placement=domain.vcpu.placement;} (toString domain.vcpu.count))
+            (elem "os" {}
+                ([
+                    (elem "type" {arch = domain.os.arch;machine = domain.os.machine;} domain.os.type)
+                ] ++
+                (map (x: elem "boot" {dev=x;} "") domain.os.boot) ++
+                [
+                    (elem "bootmenu" {enable = yesno domain.os.bootmenu.enable;} "")
+                ]
+                )
+            )
+            (elem "features" {}
+                [
+                    (opt domain.features.acpi (elem "acpi" {} ""))
+                    (opt domain.features.apic (elem "apic" {} ""))
+                ]
+            )
+            (elem "cpu" {mode=domain.cpu.mode;check=domain.cpu.check;migratable=onoff domain.cpu.migratable;}
+                [
+                    (elem "topology"
+                        {
+                            sockets=toString domain.cpu.topology.sockets;
+                            dies=toString domain.cpu.topology.dies;
+                            cores=toString domain.cpu.topology.cores;
+                            threads=toString domain.cpu.topology.threads;
+                        } ""
+                    )
+                ]
+            )
+            (elem "clock" {offset=domain.clock.offset;}
+                (map (t: elem "timer"
+                    {
+                        name = t.name;
+                    } "") domain.clock.timers)
+            )
+            (elem "on_poweroff" {} domain.on.poweroff)
+            (elem "on_reboot" {} domain.on.reboot)
+            (elem "on_crash" {} domain.on.crash)
+            (elem "pm" {}
+                [
+                    (elem "suspend-to-mem" {enabled=yesno domain.pm.suspend-to-mem;} "")
+                    (elem "suspend-to-disk" {enabled=yesno domain.pm.suspend-to-disk;} "")
+                ]
+            )
+            (elem "devices" {}
+                [
+
+                ]
+            )
         ]
     );
 in
--- xml.nix
+++ xml.nix
@@ -1,28 +1,42 @@
+with builtins;
 let
     indentLine = i: s:
         if i == 0 then s + "\n" else "  " + indentLine (i - 1) s;
 
     quoteAttr = s: "'" + s + "'"; # TBD
 
-    attrs = with builtins;
-        aa: concatStringsSep "" (map (n: " " + n + "=" + quoteAttr (getAttr n aa)) (attrNames aa));
+    attrs = aa: concatStringsSep "" (map (n: " " + n + "=" + quoteAttr (getAttr n aa)) (attrNames aa));
 
     escapeText = s: s; # TBD
 
+    concat = concatStringsSep "";
+
+    none = i: "";
+
+    opt = t: e:
+        if t then e else none;
+
+    many = ee: i: concat (map (e: e i) ee);
+
     elem = etype: aa: body: i:
-        if builtins.isString body
-        then indentLine i ("<" + etype + attrs aa + ">" + escapeText body + "</" +etype + ">")
-        else if builtins.isList body
+        if isString body
+        then if body == ""
+            then indentLine i ("<" + etype + attrs aa + "/>")
+            else indentLine i ("<" + etype + attrs aa + ">" + escapeText body + "</" +etype + ">")
+        else if isList body
         then
             indentLine i ("<" + etype + attrs aa + ">") +
-            builtins.concatStringsSep "" (builtins.map (e: e (i + 1)) body) +
+            many body (i + 1) +
             indentLine i ("</" +etype + ">")
-        else builtins.throw "XML: not text or list"
+        else throw "XML: not text or list"
         ;
 
     toText = e: e 0;
 in
 {
+    inherit none;
+    inherit opt;
+    inherit many;
     inherit elem;
     inherit toText;
 }
