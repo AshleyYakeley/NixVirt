210f1425de9ac95225d9c34619223248ee5ca72c
Ashley Yakeley <ashley@semantic.org>
2024-02-15 20:10:05 -0800

    virtio-win.iso: volume label

--- guest-install.nix
+++ guest-install.nix
@@ -1,5 +1,5 @@
 packages:
 {
   virtio-win.iso =
-    packages.runCommand "virtio-win.iso" { } "${packages.cdrtools}/bin/mkisofs -o $out ${packages.virtio-win}";
+    packages.runCommand "virtio-win.iso" { } "${packages.cdrtools}/bin/mkisofs -v VIRTIO-WIN -o $out ${packages.virtio-win}";
 }
