05c171eafc1a1f178be1eceed2a1a273abf50ebb
Ashley Yakeley <ashley@semantic.org>
2024-02-22 02:10:45 -0800

    nixvirt.py: fix

--- tool/nixvirt.py
+++ tool/nixvirt.py
@@ -136,7 +136,7 @@ class NetworkConnection(ObjectConnection):
         for domain in domains:
             domainDef = domain.descriptionXML()
             domainDefXML = lxml.etree.fromstring(domainDef)
-            intfs = domainDefXML.xpath("/domain/devices/interface")
+            intfs = domainDefXML.xpath("/domain/devices/interface/source@bridge")
             for intf in intfs:
                 self.vreport(uuid,"interface: " + lxml.etree.tostring(intf).decode("utf-8"))
         return []
