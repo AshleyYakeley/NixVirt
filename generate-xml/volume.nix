cd8fbe868b87038f543477a2c2b2834e32f4ae26
Ashley Yakeley <ashley@semantic.org>
2024-03-18 01:42:08 -0700

    generate: volumes: target format

--- generate-xml/volume.nix
+++ generate-xml/volume.nix
@@ -9,6 +9,11 @@ let
         (subelem "name" [ ] typeString)
         (subelem "capacity" [ (subattr "unit" typeString) ] (sub "count" typeInt))
         (subelem "allocation" [ (subattr "unit" typeString) ] (sub "count" typeInt))
+        (subelem "target" [ ]
+          [
+            (subelem "format" [ (subattr "type" typeString) ] [ ])
+          ]
+        )
       ];
 
 in
