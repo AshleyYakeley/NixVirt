9c8159cffa04cd3d067783abbcabaf7211b3a084
Svenum <s.ziegler@holypenguin.net>
2024-04-06 18:35:46 +0200

    add []

--- generate-xml/domain.nix
+++ generate-xml/domain.nix
@@ -232,7 +232,7 @@ let
                       (subattr "bus" typeString)
                       (subattr "slot" typeString)
                       (subattr "function" typeString)
-                    ])
+                    ][])
                   ])
                 ]
               )
