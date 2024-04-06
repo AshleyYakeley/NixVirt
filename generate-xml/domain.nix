066d6fded1fea7380303f66538137059951ec6b5
Svenum <s.ziegler@holypenguin.net>
2024-04-06 18:07:03 +0200

    add missing []

--- generate-xml/domain.nix
+++ generate-xml/domain.nix
@@ -237,7 +237,7 @@ let
               (subelem "shmem" [ (subattr "name") ] [
                 (subelem "model" [ (subattr "type" typeString) ])
                 (subelem "size" [ (subattr "unit" typeString) ] (sub "count" typeInt))
-              ])
+              ][])
               (subelem "interface"
                 [
                   (subattr "type" typeString)
