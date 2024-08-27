cba9af0b1017259a46a956a169aed4a707d38b3a
DrymarchonShaun <40149778+DrymarchonShaun@users.noreply.github.com>
2024-08-26 17:50:51 -0700

    chore: update nixpkgs to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
           type = "github";
           owner = "NixOS";
           repo = "nixpkgs";
-          ref = "nixos-23.11";
+          ref = "nixos-24.05";
         };
 
       nixpkgs-ovmf =
