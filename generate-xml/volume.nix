37bd362668c443ab99f4a3dbad1ee46ab78cfcd9
P.W.G <brussee@live.nl>
2025-02-16 04:22:40 +0100

    feat: extend volume xml generation

--- generate-xml/volume.nix
+++ generate-xml/volume.nix
@@ -4,14 +4,22 @@ let
 
   # https://libvirt.org/formatstorage.html
   process = with generate;
-    elem "volume" [ (subattr "type" typeString) ]
+    elem "volume" [ ]
       [
         (subelem "name" [ ] typeString)
-        (subelem "capacity" [ (subattr "unit" typeString) ] (sub "count" typeInt))
         (subelem "allocation" [ (subattr "unit" typeString) ] (sub "count" typeInt))
+        (subelem "capacity" [ (subattr "unit" typeString) ] (sub "count" typeInt))
         (subelem "target" [ ]
           [
             (subelem "format" [ (subattr "type" typeString) ] [ ])
+            (subelem "permissions" [ ]
+              [
+                (subelem "mode" [ ] (sub "octal" typeString))
+                (subelem "owner" [ ] (sub "uid" typeInt))
+                (subelem "group" [ ] (sub "gid" typeInt))
+                (subelem "label" [ ] (sub "MAC" typeString))
+              ]
+            )
             (subelem "compat" [ ] typeString)
             (subelem "nocow" [ ] [ ])
             (subelem "clusterSize" [ (subattr "unit" typeString) ] (sub "count" typeInt))
@@ -19,6 +27,20 @@ let
               [
                 (subelem "lazy_refcounts" [ ] [ ])
                 (subelem "extended_l2" [ ] [ ])
+              ]
+            )
+          ]
+        )
+        (subelem "backingStore" [ ]
+          [
+            (subelem "path" [ ] typeString)
+            (subelem "format" [ (subattr "type" typeString) ] [ ])
+            (subelem "permissions" [ ]
+              [
+                (subelem "mode" [ ] (sub "octal" typeString))
+                (subelem "owner" [ ] (sub "uid" typeInt))
+                (subelem "group" [ ] (sub "gid" typeInt))
+                (subelem "label" [ ] (sub "MAC" typeString))
               ]
             )
           ]
