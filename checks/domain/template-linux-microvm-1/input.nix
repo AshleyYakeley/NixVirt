15a93bd17b8b452a0dbe3b7b88f9650102358cc6
GabrielFox.Dev <inbox@gabrielfox.dev>
2026-05-09 14:51:30 -0300

    feat: add separate microvm template

---
+++ checks/domain/template-linux-microvm-1/input.nix
@@ -0,0 +1,7 @@
+lib: lib.domain.templates.linux-microvm
+{
+  name = "test-microvm";
+  uuid = "3a7b5c1d-e2f4-4a6b-8c0d-1e2f3a4b5c6d";
+  storage_vol = /Storage/MyHD.qcow2;
+  cloud_init_vol = /Storage/cloud-init.img;
+}
--- templates/domain.nix
+++ templates/domain.nix
@@ -5,5 +5,6 @@ in
 base //
 {
   linux = import domain/linux.nix stuff;
+  linux-microvm = import domain/linux-microvm.nix stuff;
   windows = import domain/windows.nix stuff;
 }
